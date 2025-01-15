c8f18a570e83275ed96ecdfa785e808b66014b7f
Semih Ahmet İlgün <4silgun@gmail.com>
2025-01-15 11:59:16 +0300

    second commit

---
+++ BagKur/AppDelegate.swift
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e290b0feacfba2b565dc83ad53677a398286f49d1edf66cbd5ec51158e8ba541
+size 464
---
+++ BagKur/Config/Enums.swift
@@ -0,0 +1,20 @@
+//
+//  Enums.swift
+//  Bağ Kur iOS Application
+//
+//  Created by Semih Ahmet İlgün on 29.12.2024.
+//
+
+import Foundation
+
+enum CollectionCellIdentifier {
+  static let onboarding = "OnboardingCollectionViewCell"
+  static let activitiesBanner = "BannerCollectionViewCell"
+  static let activities = "ActivitiesCollectionViewCell"
+}
+
+enum TableCellIdentifier {
+  static let home = "HomeTableViewCell"
+  static let activitiesBanner = "BannerTableViewCell"
+  static let activityGroup = "ActivityGroupTableViewCell"
+}
